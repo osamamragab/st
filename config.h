e3d0e7325ff8f3c8574eb28523dd2c1cfb28d45f
Osama Ragab <theosamaragab@gmail.com>
2023-09-04 04:54:11 +0300

    set font to IBM Plex Mono

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "IBM Plex Mono:pixelsize=18:antialias=true:autohint=true:weight=bold";
 /* Spare fonts */
 static char *font2[] = {
 	"Amiri:pixelsize=17.7:antialias=true:autohint=true",
