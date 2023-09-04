debe38ffea0d0342079762a66267f5d5ee882307
Osama Ragab <theosamaragab@gmail.com>
2023-09-04 20:23:33 +0300

    fix

--- config.h
+++ config.h
@@ -135,9 +135,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-static unsigned int defaultfg = 258;
-static unsigned int defaultbg = 259;
-static unsigned int defaultcs = 256;
+unsigned int defaultfg = 258;
+unsigned int defaultbg = 259;
+unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
 
 /*
