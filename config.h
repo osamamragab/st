73852f65a5742c9b475dc3cdf21d0bb0a8530622
Osama Ragab <theosamaragab@gmail.com>
2023-09-04 05:27:06 +0300

    change alpha values

--- config.h
+++ config.h
@@ -100,8 +100,8 @@ char *termname = "st-256color";
 unsigned int tabspaces = 2;
 
 /* bg opacity */
-float alpha = 0.8;
-float alphaOffset = 0.0;
+float alpha = 0.9;
+float alphaOffset = 0.2;
 float alphaUnfocus;
 
 /* Terminal colors (16 first used in escape sequence) */
